cf585efa501ec8d87fb391120dfe78729462644e
reindefox <53406744+reindefox@users.noreply.github.com>
2024-04-25 19:06:54 +0300

    Authorization activity

--- app/src/main/java/com/reindefox/homelibrary/activity/AuthorizationActivity.java
+++ app/src/main/java/com/reindefox/homelibrary/activity/AuthorizationActivity.java
@@ -1,12 +1,23 @@
 package com.reindefox.homelibrary.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.reindefox.homelibrary.databinding.ActivityAuthorizationBinding;
 
+/**
+ * Класс активности авторизации пользователя
+ */
 public class AuthorizationActivity extends AppCompatActivity {
 
     private ActivityAuthorizationBinding binding;
@@ -17,5 +28,28 @@ public class AuthorizationActivity extends AppCompatActivity {
         binding = ActivityAuthorizationBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        setupSignUpText();
+    }
+
+    private void setupSignUpText() {
+        SpannableString spannableString = new SpannableString(binding.signupText.getText());
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+
+            }
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(false);
+            }
+        };
+
+        spannableString.setSpan(clickableSpan, 20, 38, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        binding.signupText.setText(spannableString);
+        binding.signupText.setMovementMethod(LinkMovementMethod.getInstance());
+        binding.signupText.setHighlightColor(Color.TRANSPARENT);
     }
 }
--- app/src/main/java/com/reindefox/homelibrary/activity/MainActivity.java
+++ app/src/main/java/com/reindefox/homelibrary/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.reindefox.homelibrary.activity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,8 +16,18 @@ import com.reindefox.homelibrary.databinding.ActivityMainBinding;
  */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Биндинг элемента
+     */
     public ActivityMainBinding binding;
 
+    /**
+     * Базовая инициализация компонента
+     *
+     * @param savedInstanceState Если действие повторно инициализируется после предыдущего закрытия,
+     *                          то этот пакет содержит данные,
+     *                          которые оно последним предоставило в {@link #onSaveInstanceState}.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,18 +40,42 @@ public class MainActivity extends AppCompatActivity {
                 .load(R.drawable.fox_loading)
                 .into(binding.foxLoading);
 
+        // TODO Статус бар всегда видно должно быть
         // TODO подключение к БД и потом редирект на страницу авторизации
+        if (!createDatabaseConnection()) {
+            Glide.with(view)
+                    .load(R.drawable.baseline_error_outline_24)
+                    .into(binding.foxLoading);
 
-        // TODO
-        // initializeAuthorizationActivity();
+            binding.loadingText.setText(R.string.loading_db_fail);
+
+            return;
+        }
+
+        initializeAuthorizationActivity();
     }
 
-    private void createDatabaseConnection() {
-
+    /**
+     * Установка подключения к БД
+     *
+     * @return Состояние подключения
+     */
+    private boolean createDatabaseConnection() {
+        return true;
     }
 
+    /**
+     * Инициализация активности с авторизацией
+     */
     private void initializeAuthorizationActivity() {
         Intent intent = new Intent(this, AuthorizationActivity.class);
+
+        ActivityOptions options = ActivityOptions
+                .makeSceneTransitionAnimation(this, findViewById(R.id.bookImage), "robot");
+
         startActivity(intent);
+
+        // Завершение активности загрузки
+        finish();
     }
 }
